e19bf0e735cf0347dcf3d0310818612059855743
hag007 <hagai.levi.007@gmail.com>
2021-06-03 07:27:01 +0000

    fix commit

---
+++ src/constants.py
@@ -0,0 +1,80 @@
+import json
+import os
+
+# import matplotlib as mpl
+# font = {'size'   : 22}
+# mpl.rc('font', **font)
+# mpl.rc('xtick', labelsize=30)    # fontsize of the tick labels
+# mpl.rc('ytick', labelsize=30)
+# mpl.rc('axes', labelsize=22)
+# mpl.rc('legend', fontsize=20)
+
+ALGOS_ACRONYM={"jactivemodules_greedy":"jAM_greedy",
+               "jactivemodules_sa": "jAM_SA",
+               "DOMINO": "DOMINO",
+               "DOMINO2": "DOMINO",
+               "netbox": "NetBox",
+               "keypathwayminer_INES_GREEDY": "KPM",
+               "hotnet2": "HotNet2",
+               "bionet": "Bionet",
+               "netbox3": "NetBox",
+               "DOMINO3": "DOMINO",
+               "netbox2_string" : "NetBox",
+               "DOMINO4": "DOMINO" 
+               }
+
+ALGOS_NAMES=list(set(ALGOS_ACRONYM.values()))
+ALGOS=ALGOS_ACRONYM.keys() # sorted(ALGOS_ACRONYM.keys())
+# COLORDICT = {a: sns.color_palette("bright", n_colors=len(ALGOS_NAMES))[ALGOS_NAMES.index(ALGOS_ACRONYM[a])] for a in ALGOS_ACRONYM}
+# PATCHES = [Line2D([0], [0], marker='o', color='gray', label=ALGOS_ACRONYM[a], markersize=12, markerfacecolor=c, alpha=0.7) for a, c in COLORDICT.iteritems()]
+
+DATASETS_ACRONYM={"Breast_Cancer.G50":"BC",
+                  "Crohns_Disease.G50":"CD",
+                  "Schizophrenia.G50":"SCZ",
+                  "Triglycerides.G50":"TRI",
+                  "Type_2_Diabetes.G50":"T2D",
+                  "Coronary_Artery_Disease.G50":"CAD",
+                  "Bone_Mineral_Density.G50":"BMD",
+                  "Height1.G50":"Height",
+                  "Alzheimer.G50":"ALZ",
+                  "Age_Related_Macular_Degeneration.G50":"AMD",
+                  "Atrial_Fibrillation.G50":"AF",
+
+                  "TNFa_2":"TNFa",
+                  "HC12":"HC",
+                  "ROR_1":"ROR",
+                  "SHERA":"SHERA",
+                  "SHEZH_1":"SHEZH",
+                  "ERS_1":"ERS",
+                  "IEM":"IEM",
+                  "APO":"APO",
+                  "CBX":"CBX",
+                  "IFT":"IFT",}
+
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+PATH_TO_CONF = "../config/conf.json"
+config_json = json.load(open(os.path.join(dir_path, PATH_TO_CONF)))
+
+BASE_PROFILE= config_json['BASE_PROFILE']
+DATASETS_DIR= os.path.join(BASE_PROFILE, "datasets")
+NETWORKS_DIR = os.path.join(BASE_PROFILE, "networks")
+TEMPLATES_DIR = os.path.join(BASE_PROFILE, "templates")
+DICTIONARIES_DIR = os.path.join(BASE_PROFILE, "dictionaries")
+OUTPUT_GLOBAL_DIR = os.path.join(BASE_PROFILE, "report")
+GO_DIR = os.path.join(BASE_PROFILE, "go")
+CACHE_GLOBAL_DIR = os.path.join(BASE_PROFILE, "cache_global")
+TRUE_SOLUTIONS_DIR = os.path.join(BASE_PROFILE, "true_solutions")
+ROBUSTNESS_SOLUTIONS_DIR= os.path.join(BASE_PROFILE, "robustness_solutions")
+LIST_DIR = os.path.join(BASE_PROFILE, "list")
+REPOS_DIR = os.path.join(BASE_PROFILE, "repos")
+RAW_DIR = os.path.join(BASE_PROFILE, "raw")
+
+
+ENSEMBL_TO_GENE_SYMBOLS = "ensembl2gene_symbol.txt"
+ENSEMBL_TO_ENTREZ = "ensembl2entrez.txt"
+
+GO_OBO_URL = 'http://purl.obolibrary.org/obo/go/go-basic.obo'
+GO_ASSOCIATION_GENE2GEO_URL = 'https://ftp.ncbi.nlm.nih.gov/gene/DATA/gene2go.gz'
+GO_FILE_NAME = 'go_bp.obo' #'go-basic.obo'
+GO_ASSOCIATION_FILE_NAME = "gene2go"
---
+++ src/evaluation/mEHR.py
@@ -0,0 +1,133 @@
+import sys
+
+sys.path.insert(0, '../')
+
+import argparse
+
+import os
+import constants
+import pandas as pd
+import numpy as np
+import scipy
+from evaluation.modules_report import modules_ehr_for_solution
+
+MAX_N_MODULES_TH = 20
+SCATTER_FACTOR = 40
+
+
+def calc_emp_pval(cur_rv, cur_dist):
+    pos = np.size(cur_dist) - np.searchsorted(np.sort(cur_dist), cur_rv, side='left')
+
+    return pos / float(np.size(cur_dist))
+
+
+def plot_modules_ehr_summary(prefix, datasets, algos):
+    df_statistics = pd.DataFrame()
+    for cur_ds in datasets:
+        for cur_alg in algos:
+            try:
+                print("{} {}".format(cur_alg, cur_ds))
+                res = modules_ehr_for_solution(cur_alg, cur_ds, prefix=prefix,
+                                               terms_file_name=os.path.join(constants.OUTPUT_GLOBAL_DIR, "oob",
+                                                                            "emp_diff_modules_{}_{}_passed_oob.tsv"),
+                                               modules_file_name=os.path.join(constants.TRUE_SOLUTIONS_DIR,
+                                                                              "{}_{}/report/modules_summary.tsv"),
+                                               emp_ratio_th=0.5)
+            except Exception as e:
+                print("error: {}".format(e))
+                continue
+
+            if res is None:
+                continue
+            tps, fps, sig_hg_genes, sig_emp_genes, statistics, full_data = res
+
+            statistics["algo"] = cur_alg
+            statistics["dataset"] = cur_ds
+            statistics["id"] = "{}_{}".format(cur_alg, cur_ds)
+
+            df_statistics = df_statistics.append(statistics, ignore_index=True)
+
+    df_statistics = df_statistics.set_index("id")
+    df_statistics.to_csv(
+        os.path.join(constants.OUTPUT_GLOBAL_DIR, "modules_statistics_{}.tsv".format(prefix)),
+        sep='\t')
+
+    for i_a, cur_alg in enumerate(set(constants.ALGOS).intersection(algos)):
+        for i_d, cur_ds in enumerate(datasets):
+
+            solution_id = "{}_{}".format(cur_alg, cur_ds)
+
+            cur_series = df_statistics.loc[solution_id, :]
+            l_modules = []
+            for cur_modules in range(int(cur_series["n_modules"])):
+                l_modules.append((cur_series['module_{}_emp_ratio'.format(cur_modules)],
+                                  cur_series['module_{}_tp'.format(cur_modules)],
+                                  cur_series['module_{}_fp'.format(cur_modules)],
+                                  cur_series['module_{}_total'.format(cur_modules)],
+                                  cur_series['module_{}_size'.format(cur_modules)]))
+            l_modules = sorted(l_modules, key=lambda x: (x[0], x[3]), reverse=True)
+            for cur in range(min(10, len(l_modules))):
+
+                if l_modules[cur][4] < 1: continue
+
+
+
+    summary_m_ehr_mean = pd.DataFrame()
+    for n_modules_th in range(1, MAX_N_MODULES_TH + 1):
+
+        l_n_modules = []
+        EHRs = pd.DataFrame()
+        for cur_ds in datasets:
+            for cur_alg in algos:
+                solution_id = "{}_{}".format(cur_alg, cur_ds)
+                if solution_id not in list(df_statistics.index):
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "ehr"] = 0
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "algo"] = cur_alg
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "dataset"] = cur_ds
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "rank"] = 0
+                    continue
+                cur_series = df_statistics.loc[solution_id, :]
+                l_modules = []
+                for cur_modules in range(int(cur_series["n_modules"])):
+                    l_modules.append((cur_series['module_{}_emp_ratio'.format(cur_modules)],
+                                      cur_series['module_{}_tp'.format(cur_modules)],
+                                      cur_series['module_{}_fp'.format(cur_modules)],
+                                      cur_series['module_{}_total'.format(cur_modules)],
+                                      cur_series['module_{}_size'.format(cur_modules)]))
+                l_modules = sorted(l_modules, key=lambda x: (x[0], x[3]), reverse=True)
+                l_n_modules.append(len(l_modules))
+                for cur in range(min(n_modules_th, len(l_modules))):
+                    if l_modules[cur][4] < 1: continue
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "ehr"] = l_modules[cur][0]
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "algo"] = cur_alg
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "dataset"] = cur_ds
+                    EHRs.loc["{}_{}_{}".format(cur_ds, cur_alg, cur), "rank"] = cur + 1
+        
+        summary_m_ehr_mean[n_modules_th] = EHRs.groupby(by=['dataset', 'algo'])['ehr'].mean().groupby(
+            by=['algo']).mean()
+
+    summary_m_ehr_mean = summary_m_ehr_mean[np.sort(summary_m_ehr_mean.columns.values)]
+
+    summary_m_ehr_mean.to_csv(
+        os.path.join(constants.OUTPUT_GLOBAL_DIR, "evaluation", "mEHR_mean_{}.tsv".format(prefix)), sep='\t')
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description='args')
+    parser.add_argument('--datasets', dest='datasets')  # TNFa_2,HC12,SHERA,SHEZH_1,ROR_1,ERS_1,IEM Breast_Cancer.G50,Crohns_Disease.G50,Schizophrenia.G50,Triglycerides.G50,Type_2_Diabetes.G50
+    parser.add_argument('--prefix', dest='prefix', default="GE")  # PASCAL_SUM   GE
+    parser.add_argument('--base_folder_format', dest='base_folder_format',
+                        default=os.path.join(constants.OUTPUT_GLOBAL_DIR, "oob"))
+    parser.add_argument('--file_format', dest='terms_file_name_format',
+                        default="emp_diff_modules_{}_{}_passed_oob.tsv")
+    parser.add_argument('--algos', dest='algos',
+                        default="jactivemodules_greedy,jactivemodules_sa,netbox,bionet,dcem")  # ,keypathwayminer_INES_GREEDY,hotnet2,my_netbox_td
+
+    args = parser.parse_args()
+
+    datasets = args.datasets.split(",")
+    algos = args.algos.split(",")
+    prefix = args.prefix
+    plot_modules_ehr_summary(prefix, datasets, algos)
+
+
